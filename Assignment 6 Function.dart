03770b024f9f79fd5227e69456526bd3d1587eb8
Paul Muguro <107040256+Mapple254@users.noreply.github.com>
2024-03-07 20:39:08 +0300

    Add files via upload

---
+++ Assignment 6 Function.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Testing the functions
+  print('Task 1: ${addTwo(5, 4)}'); // Output: Task 1: 9.0
+  print('Task 2: ${subtractTwo(5, 2)}'); // Output: Task 2: 3.0
+  print('Task 3: ${multiplyTwo(5, 3)}'); // Output: Task 3: 15.0
+  print('Task 4: ${divideTwo(10, 2)}'); // Output: Task 4: 5.0
+  print('Task 5: ${stringLength("Hello")}'); // Output: Task 5: 5
+  print('Task 6: ${getFirstElement([1, 2, 3])}'); // Output: Task 6: 1
+}
