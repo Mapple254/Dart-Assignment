fe39b5f9802375730db073cab436d5ff38f50ffd
Paul Muguro <107040256+Mapple254@users.noreply.github.com>
2024-03-07 20:29:58 +0300

    Add files via upload

---
+++ Dart Tryout 1.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Integer data type (int)
+  int age = 25;
+  print('Age: $age'); // Output: Age: 25
+
+  // Double data type (double)
+  double temperature = 36.5;
+  print('Temperature: $temperature'); // Output: Temperature: 36.5
+
+  // String data type (String)
+  String name = 'Paul Muguro';
+  print('Name: $name'); // Output: Name: Paul Muguro
+
+  // List data type (List)
+  List<String> fruits = ['Apple', 'Banana', 'Orange'];
+  print('Fruits: $fruits'); // Output: Fruits: [Apple, Banana, Orange]
+
+  // Map data type (Map)
+  Map<String, dynamic> person = {
+    'name': 'Alice Muguro',
+    'age': 30,
+    'isStudent': true
+  };
+  print('Person: $person'); // Output: Person: {name: Alice Muguro, age: 30, isStudent: true}
+}
